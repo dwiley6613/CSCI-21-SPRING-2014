2519d7f27e094060fa9aff885a6839cd9f717ced
OpenShift guest <dwiley6613@gmail.com>
2014-01-27 12:41:54 -0500

    Added Test1.cpp demo program

---
+++ Test1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string userName;
+    cout << "What is your name? ";
+    cin >> userName;
+    
+    cout <<"Hello " << userName << "!\n" << endl;
+    
+    return 0;
+}
