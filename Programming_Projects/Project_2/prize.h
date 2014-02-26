d090d2f77abc2329e895e8d5265af42fd364e399
OpenShift guest <dwiley6613@gmail.com>
2014-02-26 16:45:54 -0500

    update pc14.cpp and makefile

--- Programming_Projects/Project_2/prize.h
+++ Programming_Projects/Project_2/prize.h
@@ -1,27 +1,21 @@
 /*Programming Project 2
  *Donald Wiley
  */
+ # include <string>
+ using namespace std;
  
 class Prize
 {
  
     public:
         //default constructor: initial values itemName ("NO NAME"), itemValue (0)
-        Prize(string itemName = "NO Name", unsigned int itemValue = 0)
-	    {
-	        prizeName = itemName;
-	        prizeValue = itemValue;
-	    }
+        Prize()
+
         //public overloaded constructor: parameters for all data members
-        Prize (string itemName);
-        
-        Prize (unsigned int itemValue);
-        
+        Prize (string itemName, unsigned int itemValue)
+
         //public destructor: empty
         ~Prize ();
-        
-        //friend overloaded operator==: returns true if the prizeName and prizeValue of the two Prizes being compared are equivalent, else return false
-        friend bool operator == (const Prize& prizeName_1, const Prize& prizeName_2);
         
         //public accessors/gets and mutators/sets for all data members
         string getPrizeName ();
@@ -29,9 +23,15 @@ class Prize
         
         unsigned int getPrizeVal();
         void setPrizeVal();
+        
+        //friend overloaded operator==: returns true if the prizeName and prizeValue of the two Prizes being compared are equivalent, else return false
+        friend: 
+            bool operator == (const Prize& prizeName_1, const Prize& prizeName_2);
+        
+        
 
     private:
-    string prizeName;
-    unsigned int prizeValue;
+        string prizeName;
+        unsigned int prizeValue;
 
-}
+};
