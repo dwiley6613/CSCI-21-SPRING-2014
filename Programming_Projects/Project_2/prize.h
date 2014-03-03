fb559b2a63b91cbf807cb8fa948029ff74017004
OpenShift guest <dwiley6613@gmail.com>
2014-03-03 11:01:30 -0500

    update programming challenge 10

--- Programming_Projects/Project_2/prize.h
+++ Programming_Projects/Project_2/prize.h
@@ -1,37 +1,34 @@
 /*Programming Project 2
  *Donald Wiley
+ *prize.h
  */
- # include <string>
- using namespace std;
- 
+#pragma once
+
+#include <string>
+using namespace std;
+
 class Prize
 {
- 
     public:
-        //default constructor: initial values itemName ("NO NAME"), itemValue (0)
-        Prize()
-
-        //public overloaded constructor: parameters for all data members
-        Prize (string itemName, unsigned int itemValue)
-
-        //public destructor: empty
+        
+        Prize ();
+        
+        Prize (string newName, unsigned int newVal);
+        
         ~Prize ();
         
-        //public accessors/gets and mutators/sets for all data members
-        string getPrizeName ();
-        void setPrizeName();
+        string getName () const;
+        void setName (string newName);
         
-        unsigned int getPrizeVal();
-        void setPrizeVal();
+        unsigned int getVal () const;
+        void setVal (unsigned int newVal);
+    
+        friend bool operator == (const Prize& prizeName_1, const Prize& prizeName_2);
         
-        //friend overloaded operator==: returns true if the prizeName and prizeValue of the two Prizes being compared are equivalent, else return false
-        friend: 
-            bool operator == (const Prize& prizeName_1, const Prize& prizeName_2);
-        
-        
-
     private:
+    
         string prizeName;
-        unsigned int prizeValue;
-
+        unsigned int prizeVal;
 };
+ 
+   
