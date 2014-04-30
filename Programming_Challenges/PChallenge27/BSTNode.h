c76655fb6731a593ea5328302d3b1218e2b9af4a
dwiley6613 <dwiley6613@gmail.com>
2014-04-30 10:34:34 -0700

    upload pc27

---
+++ Programming_Challenges/PChallenge27/BSTNode.h
@@ -0,0 +1,95 @@
+/*
+ * Donald Wiley
+ * Demonstrates the use of binary search tree 
+ * Programming Challenge 27 - UNIT TEST
+ * BSTNode.h
+ * created 4/19/2014
+ * modified 4/19/2014
+ * 
+ */
+ #pragma once
+ #include<cstdlib>
+ #include<string>
+ 
+ using namespace std;
+ 
+ class BSTNode
+ {
+	public:
+	
+		/*
+         * default constructor, sets values left and right child to (NULL) and contents(0)
+         */
+		BSTNode();
+		
+		/*
+         * overloaded constructor, int parameter assigned to contents
+		 * @param int newcontents
+         */
+        BSTNode (int newContents);
+        
+        /* 
+         * destructor, sets left and right child to (NULL)
+         */
+	    virtual ~BSTNode();
+		
+		/*
+		 * set contents of node to newContents
+		 * @param int newContents
+		 * @return void
+		 */
+		void setContents(int newContents);
+		
+		/*
+		 * get the value of contents
+		 * @return int contents
+		 */
+		 int getContents() const;
+		 
+		 /*
+		 * get the value of contents
+		 * @return int& contents
+		 */
+		 int& getContents();
+		 
+		 /*
+		  * set the left child pointer
+		  * @param BTNode* leftChild
+		  */
+		void setLeftChild(BSTNode* newLeftChild);
+		
+		/*
+		  * set the right child pointer
+		  * @param BTNode* rightChild
+		  */
+		void setRightChild(BSTNode* newRightChild);
+		
+		/*
+		 * get the pointer from left child
+		 * @return BSTNode* leftChild
+		 */
+		 BSTNode* getLeftChild() const;
+		 
+		 /*
+		 * get the pointer from left child
+		 * @return BSTNode*& leftChild
+		 */
+		 BSTNode*& getLeftChild();
+		 
+		 /*
+		 * get the pointer from right child
+		 * @return BSTNode* rightChild
+		 */
+		 BSTNode* getRightChild() const;
+		 
+		 /*
+		 * get the pointer from right child
+		 * @return BSTNode*& rightChild
+		 */
+		 BSTNode*& getRightChild();
+		
+	private:
+		BSTNode* leftChild;
+		BSTNode* rightChild;
+		int contents;
+ };
---
+++ Programming_Challenges/PChallenge27/BSTree.cpp
@@ -0,0 +1,119 @@
+/*
+ * Donald Wiley
+ * Demonstrates the use of binary search tree 
+ * Programming Challenge 27 - UNIT TEST
+ * BSTree.cpp
+ * created 4/19/2014
+ * modified 4/30/2014
+ * 
+ */
+ 
+#include"BSTree.h"
+#include"BSTNode.h"
+#include<cstdlib>
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+BSTree::BSTree()
+	:root(NULL), size(0){}
+		
+BSTree::~BSTree(){
+	clear();
+}
+	
+bool BSTree::insert(int newContents){
+	return insert(newContents, root);
+}
+
+bool BSTree::remove(int value){
+	return remove(value, root);
+}
+
+void BSTree::clear(){
+	clear(root);
+}
+
+unsigned int BSTree::getSize() const{
+	return size;
+}
+		  
+void BSTree::inOrder(){
+	inOrder(root);
+}
+	
+bool BSTree::insert(int newContents, BSTNode*& tmpRoot){
+	if(tmpRoot == NULL){
+		tmpRoot = new BSTNode(newContents);
+		size++;
+		return true;
+	}
+	else if(newContents < tmpRoot->getContents()){
+		return insert(newContents, tmpRoot->getLeftChild());
+	}
+	else if(newContents > tmpRoot->getContents()){
+		return insert(newContents, tmpRoot->getRightChild());
+	}
+	else{
+		return false;
+	}
+}
+bool BSTree::remove(int value, BSTNode*& tmpRoot){
+	BSTNode* tmpNode = tmpRoot;
+	if(tmpRoot == NULL){
+		return false;
+	}
+	else if(value < tmpRoot->getContents()){
+		return remove(value, tmpRoot->getLeftChild());
+	}
+	else if(value > tmpRoot->getContents()){
+		return remove(value, tmpRoot->getRightChild());
+	}
+	else{
+		if(tmpRoot->getLeftChild() == NULL){
+			tmpNode = tmpRoot;
+			tmpRoot = tmpRoot->getRightChild();
+			delete tmpNode;
+			size--;
+		}
+		else{
+			removeMax(tmpRoot->getContents(), tmpRoot->getLeftChild());
+		}
+		return true;
+	}
+}
+
+void BSTree::removeMax(int& value, BSTNode*& tmpRoot){
+	BSTNode* maxNode = tmpRoot;
+	if(tmpRoot->getRightChild() == NULL){
+		value = maxNode->getContents();
+		tmpRoot = maxNode->getLeftChild();
+		delete maxNode;
+		size--;
+	}
+	else{
+		removeMax(value, tmpRoot->getRightChild());
+	}
+}
+		 
+void BSTree::clear(BSTNode*& tmpRoot){
+	if(tmpRoot != NULL){
+		clear(tmpRoot->getLeftChild());
+		clear(tmpRoot->getRightChild());
+		delete tmpRoot;
+		tmpRoot = NULL;
+		size--;
+	
+	}
+}
+		 
+ void BSTree::inOrder(BSTNode* rootNode){
+	if(rootNode != NULL){
+
+		inOrder(rootNode->getLeftChild());
+		cout << rootNode->getContents() << " ";
+		inOrder(rootNode->getRightChild());
+	}
+ }
+ 
